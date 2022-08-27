1d11911c5e13a6e79259ec26d5e17ee89067285d
Alvaro Chico <alvarochico.a@gmail.com>
2022-08-27 11:24:46 -0500

    change project

--- src/constants/route-paths.ts
+++
@@ -1,9 +0,0 @@
-export const Paths = {
-  HOME: '/',
-  SIGN_IN: '/sign-in',
-  CAMPAIGNS: {
-    HOME: '/campaign/:type',
-    DETAILS: '/campaign/:type/details',
-    MANAGE: '/campaign/:type/manage',
-  },
-};
